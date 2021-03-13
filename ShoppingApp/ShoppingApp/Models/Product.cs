0ebf965ebe28fda3e0f12079695e14c031176ec0
rahul20087 <Rahul.Mistry-CW@otsuka-us.com>
2021-03-14 02:39:39 +0530

    Initial Project structure checked in

---
+++ ShoppingApp/ShoppingApp/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingApp.Models
+{
+
+    /// <summary>
+    /// Fields should be same as database
+    /// </summary>
+    class Product
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ ShoppingApp/ShoppingApp/Repository/BaseRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingApp.Repository
+{
+    /// <summary>
+    /// SQL Connection
+    /// Close Connection
+    /// Base DB Operations
+    /// </summary>
+    class BaseRepository
+    {
+        void Insert() { }
+        void Update() { }
+        void GetAll() { }
+        void GetById() { }
+    }
+}
